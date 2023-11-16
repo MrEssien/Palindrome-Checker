ee7c1362ce75ac9eccd2ff92dc911144f765d197
Your Name <Philip Essien>
2023-11-16 23:47:53 +0000

    commit

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const inputText = document.getElementById('data');
 const button = document.getElementById('btn')
 
+inputText=inputText.replace(/[\W_]/g, '');  //to strip away special characters and spaces 
+inputText= inputText.toLowerCase();  // to remove Capitals letters
 
-const palindrome =(str) => {
-    str=str.replace(/[\W_]/g, '');  //to strip away special characters and spaces 
-    str= str.toLowerCase();  // to remove Capitals letters
+const palindrome =() => {
+   // inputText=inputText.replace(/[\W_]/g, '');  //to strip away special characters and spaces 
+    //inputText= inputText.toLowerCase();  // to remove Capitals letters
     
     // then for loop to go through string to check for correct same amount of characters
-     for(let i=0; i<str.length; i+=1){
-         if(str[i] !== str[str.length-1 -i]){
-            return false;
+     for(let i=0; i<inputText.length; i+=1){
+         if(inputText[i] !== inputText[inputText.length-1 -i]){
+            document.getElementById('output').textContent = false;
             }  
         }
-        return true;
+        document.getElementById('output').textContent= true;
     
     }
 
-    console.log(palindrome(inputText))
     button.addEventListener("click", palindrome);
